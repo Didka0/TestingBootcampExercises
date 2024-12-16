23bb5348d748013c38df07c9e43dfacc519314a1
Diana Dovhun <dianadovgun4@gmail.com>
2024-12-16 17:16:03 +0200

    added an if statement exercise

---
+++ src/main/java/Exercise4.java
@@ -0,0 +1,13 @@
+public class Exercise4 {
+    public static void main (String [] args) {
+        String name = "Valq";
+        boolean isStudent = true;
+        double grade = 5.5;
+        System.out.println(name);
+        System.out.println(isStudent);
+        if(isStudent) {
+            System.out.println(grade);
+        }
+
+    }
+}
