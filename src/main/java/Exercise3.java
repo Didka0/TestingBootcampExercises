f37f256de8245e1e28f5a3cfbdf71a930ddcd8ce
Diana Dovhun <dianadovgun4@gmail.com>
2024-12-16 16:53:47 +0200

    add double numbers

--- src/main/java/Exercise3.java
+++ src/main/java/Exercise3.java
@@ -1,8 +1,8 @@
 public class Exercise3 {
     public static void main (String [] args) {
-        int a = 5;
-        int b = 4;
-        int c = a+b;
+        double a = 5.5;
+        int b = 6;
+        double c = a+b;
         System.out.println(c);
     }
 }
