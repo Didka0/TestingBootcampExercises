b259bf7c035d99639e7848779c0d3a3a158f9fe3
Diana Dovhun <dianadovgun4@gmail.com>
2024-12-16 16:38:08 +0200

    Added Exercise 2

---
+++ src/main/java/Exercise2.java
@@ -0,0 +1,10 @@
+public class Exercise2 {
+    public static void main (String [] args){
+        String studentName = "Diana";
+        final int graduationYear = 2019;
+        char letterGrade = 'A';
+        System.out.println(studentName);
+        System.out.println(graduationYear);
+        System.out.println(letterGrade);
+    }
+}
