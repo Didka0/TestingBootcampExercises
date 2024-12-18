b76b83a2aea33443a52b346b72b9e24c0f0c428f
Diana Dovhun <dianadovgun4@gmail.com>
2024-12-18 23:09:42 +0200

    Aded exercise 5

---
+++ src/main/java/Exercise5.java
@@ -0,0 +1,12 @@
+public class Exercise5 {
+    public static void main(String [] args) {
+        int height = 149;
+        int age = 18;
+
+        if (age >= 18 && height >= 150)  {
+            System.out.print(" Welcome ");
+        } else {
+            System.out.println("You must be at least 18 years old and 150cm tall");
+        }
+    }
+}
