07a6a8cc3140f3f53be76fb25dbb5acf26a907fa
Diana Dovhun <dianadovgun4@gmail.com>
2024-12-20 15:11:25 +0200

    Calculate avarage grade

---
+++ src/main/java/Exercise7.java
@@ -0,0 +1,11 @@
+public class Exercise7 {
+    public static void main(String []args) {
+        int grade1 = 6;
+        int grade2 = 8;
+        int grade3 = 5;
+
+        double total = (double) (grade1 + grade2 + grade3)/3;
+        System.out.println("Your total grade is: " + total);
+
+    }
+}
