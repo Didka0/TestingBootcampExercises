e8244d68c1fefd1317495c3c789a4dbe858f11f4
Diana Dovhun <dianadovgun4@gmail.com>
2024-12-20 15:26:40 +0200

    Testing the increment operator

---
+++ src/main/java/Exercise8.java
@@ -0,0 +1,10 @@
+public class Exercise8 {
+    public static void main (String [] args){
+        int i = 6;
+        System.out.println("Original i = " + i);
+        System.out.println("++i = " + ++i);
+        System.out.println("i++ = " + i++);
+        System.out.println("Final i = " + i);
+    }
+
+}
