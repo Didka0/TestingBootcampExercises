159497092d86a5b241594d70228b65e17bbecf15
Diana Dovhun <dianadovgun4@gmail.com>
2025-01-06 23:05:36 +0200

    Nested If

---
+++ src/main/java/Exercise12.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Exercise12 {
+    public static void main (String [] args) {
+        int age = 20;
+        if (age > 0){
+            if(age > 18){
+                System.out.println("Welcome to the website");
+            } else {
+                System.out.println("You are too young to register");
+            }
+}       else {
+            System.out.println("Invalid age");
+        }
+        }
+    }
