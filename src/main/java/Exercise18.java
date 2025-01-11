9b56462bf094d2f9d50b4400c935e6e6e637a202
Diana Dovhun <dianadovgun4@gmail.com>
2025-01-11 22:52:36 +0200

    Do...While loop

---
+++ src/main/java/Exercise18.java
@@ -0,0 +1,10 @@
+public class Exercise18 {
+    public static void main(String [] args){
+        int i = 0;
+        do
+        {
+          System.out.println("The value of i is: "+ i);
+          i++;
+        } while (i <=10);
+    }
+}
