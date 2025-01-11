f8a7674da2c1b2075e40faecdaba5765d7a22741
Diana Dovhun <dianadovgun4@gmail.com>
2025-01-11 22:35:59 +0200

    While loop

---
+++ src/main/java/Exercise17.java
@@ -0,0 +1,15 @@
+public class Exercise17 {
+    public static void main (String [] args){
+        int x = 0;
+        while ( x <= 10){
+            System.out.println(x);
+            x++;
+        }
+        System.out.println(" And back");
+        int y = 10;
+        while ( y >=0 ){
+            System.out.println(y);
+            y--;
+        }
+    }
+}
